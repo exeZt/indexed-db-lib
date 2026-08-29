f189dd2a85b2445a98128f05ee596456bd15e7f1
exezt- <exestman@gmail.com>
2026-08-29 10:30:32 +0300

    removed ns, normalized types, fixed package deprecated warning (es5)

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,31 @@ export interface IIndexedDataStore {
     value?: string, version?: number,
     query?: IDBValidKey | IDBKeyRange): Promise<T | boolean | null>
 }
+
+export enum IDBErrorTypes {
+    EDB_OPEN = `EDB_OPEN: Error occurred while initializing database connection`,
+    EDB_OPEN_DB = `Error occurred while opening database`
+  }
+
+  export enum DataBaseNames {
+    DB_APPLICATION = 'db_application',
+    DB_KEYS = 'db_keys',
+    DB_USER = 'db_user',
+    DB_CHAT = 'db_chat',
+  }
+
+  export type DataBaseOStores = {
+    name: string;
+    ai?: boolean;
+    key_path?: string | Array<string>;
+    index?: { name: string; opts: any }
+  };
+
+  export type DataBase = { [k : string ]: Array<DataBaseOStores> };
+
+  type DB = DataBase; // Type Alias
+
+
+  export type DataBasePool<T = DataBaseNames> = {
+    [a in keyof DataBaseNames]: IDBOpenDBRequest;
+  }
